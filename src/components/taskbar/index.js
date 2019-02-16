887e877e711fa220d81f40ea9280bbebed6618d2
unknown <mjawwadiqbal22@gmail.com>
2019-02-16 07:07:30 -0800

    --25 min

--- src/components/taskbar/index.js
+++ src/components/taskbar/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {addTask} from '../../actions/'
 
 class TaskBar extends React.Component{
     render(){
         return(
             <div>
                 <input placeholder="add your task here" ref='task'type="text"/>
-            <button >Add Task</button>
+            <button onClick={()=>this.props.addTask(this.refs.task.value)}>Add Task</button>
             </div>
         )
     }
+};
+function mapDispatchToProps(dispatch){
+    return bindActionCreators({addTask},dispatch);
 }
-
-export default TaskBar
+export default connect(()=>{},)(TaskBar)
